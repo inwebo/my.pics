3955be286adb6a4a508731542d61d5f716d3abd1
inwebo <inwebo@inwebo-PC>
2014-10-09 18:30:26 +0200

    Refactoring

--- core/img/edit/class.edit.php
+++ core/img/edit/class.edit.php
@@ -1,299 +1,187 @@
 <?php
 
-/**
- * Yeah
- * @package LibreMVC
- * @subpackage Image
- */
-
 namespace LibreMVC\Img;
 
-use LibreMVC\Img as Img;
+use LibreMVC\Img;
 
-/**
- * Classe de manipulation d'objet Img. Toutes les méthodes sont <code>static</code>
- */
 class Edit {
 
-    /**
-     * <p>Redimmensionne la ressource GD d'un objet \LibreMVC\Img.</p>
-     * <p>Il est possible de renseigner l'un <b>ou</b> l'autre des arguments.</p>
-     * <p>Si seulement la hauteur est renseignée, la redimension se fera proportionnellement
-     * à la hauteur. Le ratio est donc conservé. Si seulement la hauteur est renseignée,
-     * le resize sera proportionnel à la nouvelle hauteur. Si les deux arguments sont
-     * renseignés le resize sera arbirtraire.<p>
-     * 
-     * @param \LibreMVC\Img $gdObject Un objet Img
-     * @param int $new_width Largeur souhaitée.
-     * @param int $new_height Hauteur souhaitée.
-     * @return \LibreMVC\Img
-     */
-    static public function resize($gdObject, $new_width = NULL, $new_height = NULL) {
+    static public function resize( $img, $new_width = null, $new_height = null ) {
+        $src =  $img->getDriver()->getResource();
         // Resize fixed width and height
-        if (isset($new_width) && isset($new_height)) {
-            $width = $new_width;
+        if( isset( $new_width ) && isset( $new_height ) ) {
+            $width  = $new_width;
             $height = $new_height;
         }
         // Resize by new width
-        elseif (isset($new_width) && is_null($new_height)) {
+        elseif ( is_null( $new_height ) && isset( $new_width ) ) {
             // Ratio by width
-            if ($new_width > $gdObject->width) {
-                $ratio = $new_width / $gdObject->width;
-                $width = $new_width;
-                $height = round($gdObject->height * $ratio);
+            if ( $new_width > $img->getWidth() ) {
+                $ratio  = $new_width / $img->getWidth();
+                $width  = $new_width;
+                $height = round($img->getHeight() * $ratio);
             } else {
-                $ratio = $gdObject->width / $new_width;
-                $width = $new_width;
-                $height = round($gdObject->height / $ratio);
+                $ratio  = $img->getWidth() / $new_width;
+                $width  = $new_width;
+                $height = round($img->getHeight() / $ratio);
             }
         }
         // Resize by new height
-        elseif (isset($new_height) && is_null($new_width)) {
+        elseif ( isset( $new_height ) && is_null( $new_width ) ) {
             // Ratio by height
-            if ($new_height > $gdObject->height) {
-                $ratio = $new_height / $gdObject->height;
-                $width = round($gdObject->width * $ratio);
+            if ($new_height > $img->getHeight()) {
+                $ratio = $new_height / $img->getHeight();
+                $width = round($img->getWidth() * $ratio);
                 $height = $new_height;
             } else {
-                $ratio = $gdObject->height / $new_height;
-                $width = round($gdObject->width / $ratio);
+                $ratio = $img->getHeight() / $new_height;
+                $width = round($img->getWidth() / $ratio);
                 $height = $new_height;
             }
         }
 
         $image_mini = imagecreatetruecolor($width, $height);
 
-        ImageAlphaBlending($image_mini, false);
-        ImageSaveAlpha($image_mini, true);
-        imagecopyresampled($image_mini, $gdObject->resource, 0, 0, 0, 0, $width, $height, $gdObject->width, $gdObject->height);
+        imagealphablending ( $image_mini, false );
 
-        $gdObject->width = $width;
-        $gdObject->height = $height;
-        $gdObject->resource = $image_mini;
-        return $gdObject;
+        imagesavealpha($image_mini, true);
+        imagecopyresized( $image_mini, $src, 0, 0, 0, 0, $width , $height, $img->getWidth(), $img->getHeight() );
+
+        imagealphablending ( $image_mini, true );
+        return $image_mini;
     }
 
-    /**
-     * <p>Applique un masque d'opacité <code>$mask</code> sur l'image courante</p>
-     * <p>Attention <code>$mask</code> <b>DOIT</b> être une image PNG avec canal ALPHA (png 24 bits)</p>
-     *
-     * @param \LibreMVC\Img Un objet Img
-     * @param string $mask Chemin d'accés au fichier mask.
-     * @return \LibreMVC\Img
-     */
-    static public function mask($gdObject, $mask) {
-        $gdMask = Img::load($mask);
-        for ($i = 0; $i < $gdObject->width; ++$i) {
-            for ($j = 0; $j < $gdObject->height; ++$j) {
-                $pxl_alpha = imagecolorsforindex($gdMask->resource, imagecolorat($gdMask->resource, $i, $j));
-                $pxl_color = imagecolorsforindex($gdObject->resource, imagecolorat($gdObject->resource, $i, $j));
-                $color = imagecolorallocatealpha(
-                        $gdObject->resource, $pxl_color['red'], $pxl_color['green'], $pxl_color['blue'], $pxl_alpha['alpha']);
-                imagesetpixel($gdObject->resource, $i, $j, $color);
-            }
-        }
-        imagesavealpha($gdObject->resource, true);
-        return $gdObject;
-    }
+    static public function mask( Img $img, $path ){
 
-    /**
-     * <p>Applique un motif sur l'ensemble d'une image.</p>
-     * <p>Attention <code>$pattern</code> <b>DOIT</b> être une image PNG avec canal ALPHA (png 24 bits)</p>
-     *
-     * @param \LibreMVC\Img Un objet Img
-     * @param string $pattern Chemin d'accés au fichier mask.
-     * @return \LibreMVC\Img
-     */
-    static public function pattern($gdObject, $pattern) {
+        try {
+            $img->convertTo('png');
+            $layer = Img::load($path);
+            $imgResource = $img->getDriver()->getResource();
 
-        $gdPattern = Img::load($pattern);
+            imagealphablending($imgResource, false);
 
-        static $x = 0;
-        static $y = 0;
 
-        $nbrColumn = floor($gdObject->width / $gdPattern->width);
-        $nbrRow = floor($gdObject->height / $gdPattern->height);
+            $layerResource = $layer->getDriver()->getResource();
 
-        for ($i = 0; $i <= $nbrRow; $i++) {
-
-            for ($j = 0; $j <= $nbrColumn; $j++) {
-                imagecopy($gdObject->resource, $gdPattern->resource, $x, $y, 0, 0, $gdPattern->width, $gdPattern->height);
-                $x += $gdPattern->width;
+            if( $layer->getWidth() !== $img->getWidth() || $layer->getHeight() !== $img->getHeight()) {
+                $layer->resize($img->getWidth(),$img->getHeight());
             }
 
-            $x = 0;
-            $y += $gdPattern->height;
+            for ($i = 0; $i < $img->getWidth(); ++$i) {
+                for ($j = 0; $j < $img->getHeight(); ++$j) {
+
+                    $pxl_alpha = imagecolorsforindex(
+                        $layerResource,
+                        imagecolorat(
+                            $layerResource,
+                            $i, $j
+                        )
+                    );
+
+                    $pxl_color = imagecolorsforindex(
+                        $imgResource,
+                        imagecolorat(
+                            $imgResource,
+                            $i, $j
+                        )
+                    );
+
+                    $color = imagecolorallocatealpha(
+                        $imgResource,
+                        $pxl_color['red'], $pxl_color['green'], $pxl_color['blue'], $pxl_alpha['alpha']
+                    );
+
+                    //var_dump($pxl_alpha['alpha'] );
+                    //echo 'Origin alpha : ' . $pxl_color['alpha'] . ' <br>';
+                    //echo 'New alpha : ' . $pxl_alpha['alpha'] . ' <br>';
+                    imagesetpixel($imgResource, $i, $j, $color);
+                    //echo 'Current alpha : ' .imagecolorsforindex($imgResource, imagecolorat($imgResource, $i, $j))['alpha'] ;
+                    //echo '<br>--------<br>';
+                    //var_dump(imagecolorsforindex($imgResource, imagecolorat($imgResource, $i, $j))['alpha']);
+                }
+            }
+            //imageAlphaBlending($imgResource, true);
+
+            return $imgResource;
         }
-        return $gdObject;
+        catch(\Exception $e) {
+            var_dump($e);
+        }
+
+
     }
 
-    /**
-     * <p>Rogne une image.</p>
-     * <p>Rogne une image <code>$gdObject</code> sur une surface <code>$crop</code> depuis
-     * la position <code>$from</code> <b>ou</b> depuis l'origine (x,y) <code>$origin</code></p>
-     *
-     * @param \LibreMVC\Img Un objet Img
-     * @param array $crop Tableau pour la largeur et la hauteur du rognage souhaité.
-     * @param string $from Position du rognage, parmi : TOP_LEFT, TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM
-     * BOTTOM_LEFT, LEFT
-     * @param array $origin SI <code>$from</code> n'est pas renseigné commence aux coordonnées x,y
-     * @return \LibreMVC\Img
-     * @throws Exception Si <code>$flag</code> n'est pas reconnus.
-     */
-    static public function crop($gdObject, $crop = array(null, null), $from = "CENTER", $origin = array("x" => null, "y" => null)) {
-        if ($crop[0] >= $gdObject->width || $crop[1] >= $gdObject->height) {
-            trigger_error("Outbounds cropping");
-        }
+    static public function pattern(Img $img, $path){
+        try {
+            //imagealphablending ( $img->getDriver()->getResource(), false );
 
-        $image_mini = imagecreatetruecolor($crop[0], $crop[1]);
-        if ($origin['x'] != '' && $origin['y'] != '' && $from = CUSTOM) {
-            ImageAlphaBlending($image_mini, false);
-            ImageSaveAlpha($image_mini, true);
-            imagecopyresampled($image_mini, $gdObject->resource, 0, 0, $origin['x'], $origin['y'], $crop[0], $crop[1], $crop[0], $crop[1]);
-            $gdObject->resource = $image_mini;
-        } else {
-            static $x;
-            static $y;
+            $layer = Img::load($path);
 
-            switch ($from) {
+            $cols = floor($img->getWidth() / $layer->getWidth());
+            $rows = floor($img->getHeight() / $layer->getHeight());
 
-                case "TOP_LEFT":
-                    $x = 0;
-                    $y = 0;
-                    break;
+            $startX = 0;
+            $startY = 0;
 
-                case "TOP":
-                    $x = ($gdObject->width - $crop[0]) / 2;
-                    $y = 0;
-                    break;
-
-                case "TOP_RIGHT":
-                    $x = ($gdObject->width - $crop[0]);
-                    $y = 0;
-                    break;
-
-                case "RIGHT":
-                    $x = ($gdObject->width - $crop[0]);
-                    $y = ($gdObject->height - $crop[1]) / 2;
-                    break;
-
-                case "BOTTOM_RIGHT":
-                    $x = ($gdObject->width - $crop[0]);
-                    $y = ($gdObject->height - $crop[1]);
-                    break;
-
-                case "BOTTOM":
-                    $x = ($gdObject->width - $crop[0]) / 2;
-                    $y = ($gdObject->height - $crop[1]);
-                    break;
-
-                case "BOTTOM_LEFT" :
-                    $x = 0;
-                    $y = ($gdObject->height - $crop[1]);
-                    break;
-
-                case "LEFT" :
-                    $x = 0;
-                    $y = ($gdObject->height - $crop[1]) / 2;
-                    break;
-
-                case "CENTER" :
-                    $x = ($gdObject->width - $crop[0]) / 2;
-                    $y = ($gdObject->height - $crop[1]) / 2;
-                    break;
-
-                default :
-                    throw new Exception('Bad $flag, try : TOP_LEFT, TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, LEFT, CUSTOM');
-                    break;
+            // For each rows.
+            for ($i = 0; $i <= $rows; $i++) {
+                for ($j = 0; $j <= $cols; $j++) {
+                    imagecopy(
+                        $img->getDriver()->getResource(),
+                        $layer->getDriver()->getResource(),
+                        $startX, $startY,
+                        0, 0,
+                        $layer->getWidth(), $layer->getHeight()
+                    );
+                    $startX += $layer->getWidth();
+                }
+                $startX = 0;
+                $startY += $layer->getHeight();
             }
 
-            ImageAlphaBlending($image_mini, false);
-            ImageSaveAlpha($image_mini, true);
-            imagecopyresampled($image_mini, $gdObject->resource, 0, 0, $x, $y, $crop[0], $crop[1], $crop[0], $crop[1]);
-            $gdObject->resource = $image_mini;
+            return $img->getDriver()->getResource();
+        }
+        catch(\Exception $e) {
+            var_dump($e);
         }
 
-        return $gdObject;
     }
 
-    /**
-     * <p>Fusionne une image dans un objet image.</p>
-     * 
-     * @param \LibreMVC\Img Un objet Img
-     * @param string $fileToMerge Le fichier à fusionner avec l'objet courant
-     * @param string $target Position de de la fusion
-     * @param int $opacity Opacité souhaitée [0,99]
-     * @param array $origin Position personnalisée
-     * @param int $margin Une marge est elle souhaitée
-     * @return \LibreMVC\Img Un objet Img modifié.
-     * @throws Exception Si <code>$target</code> est inconnu.
-     */
-    static function merge($gdObject, $fileToMerge, $target = "CENTER", $opacity = 99, $origin = array(), $margin = NULL) {
-        $gdMerge = Img::load($fileToMerge);
+    public function crop(){}
 
-        static $x;
-        static $y;
+    public function merge(){}
 
-        switch (strtoupper($target)) {
-
-            case "TOP_LEFT":
-                $x = 0 + $margin;
-                $y = 0 + $margin;
-                break;
-
-            case "TOP":
-                $x = ( $gdObject->width - $gdMerge->width) / 2;
-                $y = 0 + $margin;
-                break;
-
-            case "TOP_RIGHT":
-                $x = ($gdMerge->width - $gdObject->width) - $margin;
-                $y = 0 - $margin;
-                break;
-
-            case "RIGHT":
-                $x = ($gdMerge->width - $gdObject->width) - $margin;
-                $y = ($gdMerge->height - $gdObject->height) / 2;
-                break;
-
-            case "BOTTOM_RIGHT":
-                $x = ($gdMerge->width - $gdObject->width) - $margin;
-                $y = ($gdMerge->height - $gdObject->height) - $margin;
-                break;
-
-            case "BOTTOM":
-                $x = ($gdMerge->width - $gdObject->width) / 2;
-                $y = ($gdMerge->height - $gdObject->height) - $margin;
-                break;
-
-            case "BOTTOM_LEFT" :
-                $x = 0 + $margin;
-                $y = ($gdMerge->height - $gdObject->height) - $margin;
-                break;
-
-            case "LEFT" :
-                $x = 0 + $margin;
-                $y = ($gdMerge->height - $gdObject->height) / 2;
-                break;
-
-            case "CENTER" :
-                $x = floor(( $gdObject->width - $gdMerge->width ) / 2);
-                $y = floor(( $gdObject->height - $gdMerge->height ) / 2);
-                break;
-
-            case "CUSTOM" :
-                $x = $origin[0];
-                $y = $origin[1];
-                break;
-
-            default :
-                throw new Exception('Bad $flag, try : TOP_LEFT, TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, LEFT, CENTER, CUSTOM');
-                break;
+    static public function getPalette( Img $img, $fastProcess = false ) {
+        $buffer = $img;
+        if($fastProcess) {
+            $buffer = $buffer->resize(150);
         }
 
-        imagecopymerge($gdObject->resource, $gdMerge->resource, $x, $y, 0, 0, $gdMerge->width, $gdMerge->height, $opacity);
-        return $gdObject;
+        $hexarray = array();
+
+        for ($y=0; $y < $buffer->getHeight(); $y++)
+        {
+            for ($x=0; $x < $buffer->getWidth(); $x++)
+            {
+                $index = imagecolorat( $buffer->getDriver(),$x,$y);
+                $Colors = imagecolorsforindex($buffer->getDriver(),$index);
+                $Colors['red']=intval((($Colors['red'])+15)/32)*32; //ROUND THE COLORS, TO REDUCE THE NUMBER OF COLORS, SO THE WON'T BE ANY NEARLY DUPLICATE COLORS!
+                $Colors['green']=intval((($Colors['green'])+15)/32)*32;
+                $Colors['blue']=intval((($Colors['blue'])+15)/32)*32;
+                if ($Colors['red']>=256)
+                    $Colors['red']=240;
+                if ($Colors['green']>=256)
+                    $Colors['green']=240;
+                if ($Colors['blue']>=256)
+                    $Colors['blue']=240;
+                $hexarray[]=substr("0".dechex($Colors['red']),-2).substr("0".dechex($Colors['green']),-2).substr("0".dechex($Colors['blue']),-2);
+            }
+        }
+        $hexarray=array_count_values($hexarray);
+        natsort($hexarray);
+        $hexarray=array_reverse($hexarray,true);
+        return $hexarray;
+
     }
 
 }
-
--- index.php
+++ index.php
@@ -1,85 +1,63 @@
 <?php
+include('../helpers/files/iio/class.iio.php');
+use LibreMVC\Img;
 
-namespace LibreMVC;
+    ini_set('display_errors', 'on');
+    include('core/img/autoload.php');
+    //$img = \LibreMVC\Img::load("http://cdn.sstatic.net/stackoverflow/img/favicon.ico?v=038622610830");
 
-include 'autoload.php';
+    $folder = './pics/';
 
+    $jpg ="./pics/chat.jpg";
+    $gif ="./pics/php.gif";
+    $png ="./pics/transparent.png";
 
-$projectName = 'My.Pictures';
-$projectVersion = '01-01-2013';
-$projectKeywords = 'inwebo';
-$projectShortDescription = 'Non destructive pictures manipulation';
-error_reporting(E_ALL);
-?>
-<?php ini_set('display_errors', TRUE); ?>
+    $ico ="http://php.net/favicon.ico";
 
-<!doctype html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="keywords" content="<?php echo $projectKeywords; ?>" />
-    <meta name="author" lang="fr" content="Inwebo" />
-    <meta name="copyright" content="Creative commons" />
-    <meta name="date" content="2012" />
-    <title><?php echo $projectName; ?></title>
-    <meta name="description" content="<?php echo $projectShortDescription; ?>">
-    <meta name="viewport" content="width=device-width">
-    <link rel="shortcut icon" href="favicon.ico">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<?php
-$img = new Img('assets/picture.jpg');
-var_dump($img);
-$serialize = serialize($img);
-$img = unserialize($serialize);
-//$img->save("public/test.png");
-var_dump($img);
+    try {
+        //$img = new \LibreMVC\Img("http://php.net/favicon.ico");
+        //$img = new \LibreMVC\Img("http://cdn.sstatic.net/stackoverflow/img/favicon.ico?v=038622610830");
+        //$img = new \LibreMVC\Img("http://static.php.net/www.php.net/images/php.gif");
+        //$img->save();
+        //$img = new \LibreMVC\Img("mages/php.gif");
+        //$img = new Img("pics/php.gif");
+        //var_dump($img);
+        //var_dump( getimagesize("pics/php.gif") );
+        //var_dump( getimagesize("http://php.net/favicon.ico") );
+        //var_dump( memory_get_usage() );
+        //$img = new Img();
+        //var_dump($img);
+        $img = new Img("http://www.cfma.org/files/PageLayoutImages/icon_social_linkedIn.jpg");
+        $img->resize(800,800);
+        //$img->save('./pics/big.jpg');
+        //var_dump($img->getPalette());
+        //$img = new Img($jpg);
+        //$img->save();
+        var_dump($img);
+        //$img = new Img('pics/transparent.png');
+        //var_dump($img);
+        //var_dump(getimagesize("t"));
+        //$img = new Img($jpg);
+    }
+    catch (\Exception $e) {
+        echo $e->getMessage();
+    }
 
-$l = Img::load('assets/picture.jpg')->extractColorPalette();
-var_dump($l);
-$i=0;
-foreach($l as $key => $value ) {
-    //echo '<div style="background-color:#'. trim($key,"'")  .'">&nbsp;</div>';
-    ++$i;
-    echo '<div style="background-color:#'. $key  .';width:50px;">' . $key . '</div>';
+/**
+ * @param $va Convert ini data 32M en octets.
+ */
+function getIniDirective( $va ) {
+
 }
+
 ?>
 <html>
-    <body>
-        <header>
-            <a name="top"></a>
-            <h1><?php echo $projectName; ?> <span id="version">version : <span><?php echo $projectVersion; ?></span></span></h1>
-        </header>
-        <div role="main">
-            <?php
-            if (isset($_GET["q"]) && $_GET['q'] == 1) {
-                echo '<h2>PHAR</h2><p><a href="module.phar">Download me</a></p>';
+<head>
 
-                try {
-                    $phar = new Phar(__DIR__ . '/phar/' . $projectName . '.phar');
-                    $phar->buildFromDirectory('core/', '/\.php$/');
-                    $phar->stopBuffering();
-                } catch (Exception $e) {
-                    echo $e->getMessage();
-                }
-            }
-            ?>
-            <h2>Pictures</h2>
-            <img src="picture.php">
-            <h2>Creation archive PHAR <a href="#top">TOP</a></h2>
-            <p>
-                <a href="http://cweiske.de/tagebuch/php-phar-files.htm" target="_blank" title="Why PHAR archive ?">Why PHAR archives ?</a>,
-                <a href="index.php?q=1" target="_self">Make <?php echo $projectName . '.phar' ?> phar archive</a>, php.ini doit être configuré correctement !
-                <br><br><br>
-                <code>
-                    phar.readonly= 0;
-                </code>
-            </p>
-        </div>
-        <footer>
-            <p>
-                <a title="Julien Hannotin" href="http://julien.hannotin.is.free.fr" target="_blank" title="Résumé">Jool</a> | <a href="http://creativecommons.org/licenses/by-nc-sa/2.0/fr/" title="Creative Commons 2"  target="_blank">creative commons 2</a> | <a title="Git repository" target="_blank" href="https://github.com/inwebo/">Github Repository</a> | <a href="#top">top</a>
-            </p>
-        </footer>
-    </body>
+</head>
+<body style="background-color: darkgray">
+<a href="pics.php" target="_blank"><img src="pics.php" /></a>
+</body>
 </html>
+<hr>
+
