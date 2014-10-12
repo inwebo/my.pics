34d8b6c4fe853da53546df4e0d3fe9a8445dfe03
inwebo <inwebo@localhost.localdomain>
2014-10-12 17:19:04 +0200

    Rename

--- core/img/autoload.php
+++ core/img/autoload.php
@@ -5,12 +5,13 @@ include('driver/jpg/class.jpg.php');
 include('driver/gif/class.gif.php');
 include('driver/bmp/class.bmp.php');
 include('driver/ico/class.ico.php');
-include('driver/ico/imagesdirs/class.imagesdirs.php');
-include('driver/ico/iconimage/bitmapinfoheader/class.bitmapinfoheader.php');
+include('driver/ico/imagemap/class.imagemap.php');
+include('driver/ico/image/header/class.header.php');
 include('imgbase/class.imgbase.php');
 include('imgactionable/class.imgactionable.php');
 include('imgeditable/class.imgeditable.php');
 include('class.img.php');
 include('edit/class.edit.php');
 include('filters/class.filters.php');
+
 
