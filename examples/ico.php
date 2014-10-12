a4b7d2942ce3e69bc92057c99e757b190a2e0c38
inwebo <inwebo@localhost.localdomain>
2014-10-12 17:21:01 +0200

    Add ico

--- examples/ico.php
+++ examples/ico.php
@@ -2,11 +2,15 @@
 use LibreMVC\Img;
 
 include('../core/img/autoload.php');
+include('../core/bin/autoload.php');
 
 try {
+    ini_set('display_errors', 'on');
     //$img = new Img('../pics/wikipedia.ico');
-    $img = new Img\Driver\Ico('../pics/wikipedia.ico');
-    var_dump($img);
+    //$img = new Img\Driver\Ico('../pics/wikipedia.ico');
+    $img = new Img\Driver\Ico('../pics/a.ico');
+
+    //var_dump($img);
     //var_dump($img->display());
     //header('Content-Type: image/ico');
     //echo file_get_contents('../pics/wikipedia.ico');
